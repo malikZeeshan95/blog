ddf18f686107aa8576ef01be004f7da5fe1b65aa
Malik Zeeshan <malikzeeshan95@gmail.com>
2017-12-13 02:03:21 -0800

    Configuration is setUp Vue commponents

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,19 +4,14 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+//dont remve it its for pull in the boostrape.js
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
-Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+//Vue.component('example', require('./components/Example.vue'));
+
+import Buefy from 'buefy';
+Vue.use(Buefy);
